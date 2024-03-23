2f54ad9bff1297d2760853ad82325c20233505ef
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2024-03-23 20:21:15 +0000

    Support Python + SQL, identified via element class name

--- editor.mjs
+++ editor.mjs
@@ -1,14 +1,31 @@
 import { EditorView, minimalSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
 import { sql } from "@codemirror/lang-sql";
+import { python } from "@codemirror/lang-python";
+
+let lang_exts = {
+  sql: sql,
+  python: python,
+};
 
 document.querySelectorAll("pre code").forEach((code) => {
+  // detect language based on element class, e.g. `lang-sql` or `lang-python`
+  let classes = code.parentNode.className.split(" ");
+  console.log(classes);
+  let exts = [minimalSetup]
+  for (let lang in lang_exts) {
+    if (classes.includes(`lang-${lang}`)) {
+      exts.push(lang_exts[lang]());
+    }
+  }
+
   let view = new EditorView({
     state: EditorState.create({
       doc: code.textContent,
-      extensions: [minimalSetup, sql()],
+      extensions: exts,
     }),
     parent: code.parentElement,
   });
+
   code.remove();
 });
